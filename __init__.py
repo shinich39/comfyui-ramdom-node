6b6cc8d667560c289c8977a8c6dff348b6eb74df
shinich39 <shinich39@gmail.com>
2024-03-31 12:06:09 +0900

    v1.0.1

--- __init__.py
+++ __init__.py
@@ -2,14 +2,14 @@
 @author: shinich39
 @title: Random Node #39
 @nickname: Random Node #39
-@version: 1.0.0
+@version: 1.0.1
 @description: Choose random node.
 """
 
 import random
 
 DEBUG = False
-VERSION = "1.0.0"
+VERSION = "1.0.1"
 WEB_DIRECTORY = "./js"
 NODE_CLASS_MAPPINGS = {}
 NODE_DISPLAY_NAME_MAPPINGS = {}
@@ -31,9 +31,18 @@ class RandomNode:
         return {
             "required": {
                 "type": (["boolean","number","int","float","string","text","image","mask","latent","conditioning","model","clip","vae",],),
-                "count": ("INT", {"default": 1, "min": 1, "max": 99, "step": 1}),
-                "input": (ANY_TYPE, )
+                "input1": (ANY_TYPE,),
             },
+            "optional": {
+                "input2": (ANY_TYPE,),
+                "input3": (ANY_TYPE,),
+                "input4": (ANY_TYPE,),
+                "input5": (ANY_TYPE,),
+                "input6": (ANY_TYPE,),
+                "input7": (ANY_TYPE,),
+                "input8": (ANY_TYPE,),
+                "input9": (ANY_TYPE,),
+            }
         }
     
     @classmethod
@@ -41,27 +50,31 @@ class RandomNode:
         return float("NaN")
     
     CATEGORY = "utils"
-    RETURN_TYPES = (ANY_TYPE,)
-    RETURN_NAMES = ("output",)
+    RETURN_TYPES = (ANY_TYPE,ANY_TYPE,ANY_TYPE,ANY_TYPE,ANY_TYPE,ANY_TYPE,ANY_TYPE,ANY_TYPE,ANY_TYPE,)
+    RETURN_NAMES = ("output1","output2","output3","output4","output5","output6","output7","output8","output9",)
     FUNCTION = "exec"
 
-    def exec(self, type, count, **inputs):
+    def exec(self, type, **inputs):
 
         if DEBUG:
             print(f"type: {type}")
-            print(f"count: {count}")
     
-        items = list(inputs.values())
-        values = [None for x in range(count)]
-        types = [type.upper() for x in range(count)]
-        for i in range(len(items)):
-            values[i] = items[i]
-        
-        random.shuffle(values)
+        values = list(inputs.values())
+        new_values = []
+        for i in range(9):
+            for v in values:
+                new_values.append(v)
 
-        self.RETURN_TYPES = tuple(types)
 
-        return tuple(values)
+        if DEBUG:
+            print(new_values)
+
+        random.shuffle(new_values)
+
+        if DEBUG:
+            print(new_values[0:9])
+
+        return tuple(new_values[0:9])
 
 NODE_CLASS_MAPPINGS["Random Node #39"] = RandomNode
 NODE_DISPLAY_NAME_MAPPINGS["Random Node #39"] = "Random Node #39"
--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ import { app } from "../../scripts/app.js";
 
 const DEBUG = false;
 const NODE_TYPE = "Random Node #39";
-const INTPUT_NAME = "input";
+const INPUT_NAME = "input";
 const OUTPUT_NAME = "output";
 
 if (DEBUG) {
-  console.log("#39 js loaded");
+  console.log("#39 loaded");
 }
 
 function chkLink(linkId, type) {
@@ -14,10 +14,11 @@ function chkLink(linkId, type) {
     return;
   }
 
-  const link = app.graph.links[parseInt(linkId)];
+  const link = app.graph.links[linkId];
   if (!link) {
     return;
   }
+
   if (link.type.toLowerCase() !== type.toLowerCase() && link.type !== "*" && type.toLowerCase() !== "*") {
     app.graph.removeLink(link.id);
   }
@@ -25,7 +26,7 @@ function chkLink(linkId, type) {
 
 function getInputs(node) {
   return node.inputs.filter(function(input) {
-    return input.name.indexOf(INTPUT_NAME) > -1;
+    return input.name.indexOf(INPUT_NAME) > -1;
   });
 }
 
@@ -35,78 +36,25 @@ function getOutputs(node) {
   });
 }
 
-function getLastInputIndex(node) {
-  let i = node.inputs.length - 1;
-  for (i; i >= 0; i--) {
-    if (node.inputs[i].name.indexOf(INTPUT_NAME) > -1) {
-      break;
-    }
-  }
-  return i;
-}
-
-function getLastOutputIndex(node) {
-  let i = node.outputs.length - 1;
-  for (i; i >= 0; i--) {
-    if (node.outputs[i].name.indexOf(OUTPUT_NAME) > -1) {
-      break;
-    }
-  }
-  return i;
-}
-
-function setInputs(node, count, type) {
-  while(getInputs(node).length !== count) {
-    if (getInputs(node).length > count) {
-      node.removeInput(getLastInputIndex(node));
-    } else {
-      node.addInput(INTPUT_NAME, type);
-    }
-  }
-
-  // set type
+function setTypes(node, type) {
   const inputs = getInputs(node);
-  let idx = 0;
   for (let i = 0; i < inputs.length; i++) {
     const input = inputs[i];
-    input.name = INTPUT_NAME + idx;
     input.type = type;
-    input.label = INTPUT_NAME;
-    input.required = true;
+    input.label = INPUT_NAME;
 
     // unlink
     chkLink(input.link, type);
-
-    idx++;
-  }
-}
-
-function setOutputs(node, count, type) {
-  while(getOutputs(node).length !== count) {
-    if (getOutputs(node).length > count) {
-      node.removeOutput(getLastOutputIndex(node));
-    } else {
-      node.addOutput(OUTPUT_NAME, type);
-    }
   }
 
-  // set type
   const outputs = getOutputs(node);
-  let idx = 0;
   for (let i = 0; i < outputs.length; i++) {
     const output = outputs[i];
-    output.name = OUTPUT_NAME + idx;
     output.type = type;
     output.label = OUTPUT_NAME;
 
     // unlink
-    if (output.links) {
-      for (const linkId of output.links) {
-        chkLink(linkId, type);
-      }
-    }
-
-    idx++;
+    chkLink(output.links, type);
   }
 }
 
@@ -119,42 +67,23 @@ function setNode(node, app) {
     return item.name == "type";
   }) : null;
 
-  const countWidget = node.widgets ? node.widgets.find(function(item) {
-    return item.name == "count";
-  }) : null;
-
-  if (!typeWidget || !countWidget) {
+  if (!typeWidget) {
     return;
   }
 
-  node._count = countWidget.value;
   node._type = typeWidget.value;
 
   if (DEBUG) {
-    console.log("Random #39 node", node);
+    console.log("#39 node", node);
   }
 
-  setInputs(node, node._count, node._type);
-  setOutputs(node, node._count, node._type);
-
-  Object.defineProperty(countWidget, "value", {
-    set: async function(value) {
-      node._count = value;
-
-      setInputs(node, value, node._type);
-      setOutputs(node, value, node._type);
-    },
-    get: function() {
-      return node._count;
-    }
-  });
+  setTypes(node, node._type);
 
   Object.defineProperty(typeWidget, "value", {
     set: async function(value) {
       node._type = value;
 
-      setInputs(node, node._count, value);
-      setOutputs(node, node._count, value);
+      setTypes(node, node._type);
     },
     get: function() {
       return node._type;
